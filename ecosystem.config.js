99711c699fc738d2bd0bb2f8f979d20edc444b70
Ben Newman <ben@bnewman.dev>
2021-03-20 11:57:43 +0000

    ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,23 +7,5 @@ module.exports = {
 			log_file: "logs/combined.log",
 			time: true,
 		},
-		{
-			script: "./service-worker/",
-			watch: ["./service-worker"],
-		},
 	],
-
-	deploy: {
-		production: {
-			user: "SSH_USERNAME",
-			host: "SSH_HOSTMACHINE",
-			ref: "origin/master",
-			repo: "GIT_REPOSITORY",
-			path: "DESTINATION_PATH",
-			"pre-deploy-local": "",
-			"post-deploy":
-				"npm install && pm2 reload ecosystem.config.js --env production",
-			"pre-setup": "",
-		},
-	},
 };
